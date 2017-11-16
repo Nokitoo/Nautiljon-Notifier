15ca52d4019e21f9f29a5aad35675b0666cab574
Nokitoo <guillaume.labey@epitech.eu>
2017-11-17 00:24:12 +0100

    Modify build script to build inno setup .exe

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-# Use -O to set __debug__ to False
-rm -rf build dist && python -O -m PyInstaller scripts/build.spec
+rm -rf build dist &&
+
+if [ "$1" = "debug" ]; then
+    python -m PyInstaller scripts/build.spec
+else
+    # Use -O to set __debug__ to False
+    python -O -m PyInstaller scripts/build.spec
+fi  && iscc build.iss
