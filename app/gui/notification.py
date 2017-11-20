f9b832b49d974fcb8b3eed99b74059973df5e655
Nokitoo <guillaume.labey@epitech.eu>
2017-11-21 00:14:13 +0100

    Queue notifications

--- app/gui/notification.py
+++ app/gui/notification.py
@@ -1,7 +1,7 @@
 import logging
 
 from PyQt5.QtWidgets import QDialog, QApplication
-from PyQt5.QtCore import Qt, QPropertyAnimation, QSize
+from PyQt5.QtCore import Qt, QPropertyAnimation, QSize, QTimer
 from PyQt5.QtGui import QIcon
 from gui.notification_ui import Ui_Dialog as NotificationDialog
 
@@ -9,10 +9,13 @@ from config import assets
 
 class Notification(QDialog, NotificationDialog):
     mainWindow = None
+    queue = []
+    isNotificationDisplayed = False
 
-    def __init__(self, parent):
+    def __init__(self, parent, timeout):
         super().__init__(parent)
         self.setupUi(self)
+        self.timeout = timeout
 
         # Notification offset
         marginBottom = 50.0; # Desktop footer + margin
@@ -31,18 +34,38 @@ class Notification(QDialog, NotificationDialog):
              self.height()
         )
 
+        self.destroyed.connect(self.onNotificationDestroyed)
+
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
-            self.deleteLater()
+            self.destroyNotification()
+
+    def onNotificationDestroyed(self):
+        Notification.isNotificationDisplayed = False
+        if len(Notification.queue) > 0:
+            notification = Notification.queue.pop(0)
+            notification.show()
+            Notification.isNotificationDisplayed = True
+
+    def destroyNotification(self):
+        self.destroy()
+        self.onNotificationDestroyed()
+
+    def show(self):
+        super().show()
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.destroyNotification)
+        self.timer.start(self.timeout)
+
 
     @staticmethod
-    def create(title, message, pixmap):
+    def create(title, message, pixmap, timeout=5000):
         if not Notification.mainWindow:
             logging.error('Need to set mainwindow before calling Notification.show()')
             return
 
-        notification = Notification(Notification.mainWindow)
+        notification = Notification(Notification.mainWindow, timeout)
         notification.title.setText(title)
         notification.message.setText(message)
 
@@ -50,4 +73,8 @@ class Notification(QDialog, NotificationDialog):
         height = pixmap.height();
         notification.icon.setPixmap(pixmap.scaled(width, height, Qt.KeepAspectRatio));
 
-        notification.show()
+        if not Notification.isNotificationDisplayed:
+            notification.show()
+            Notification.isNotificationDisplayed = True
+        else:
+            Notification.queue.append(notification)
